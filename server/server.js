b2ea32a5035f1d89db529dc3083055e5b4413a21
SANTECXSOFTWARES <santecxsoftwares@gmail.com>
2021-12-18 01:31:18 +0530

    Added Server and mongo DB connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const router = require('./routes/routes.js');
 
+dotenv.config();
+//API Config
+const app = express()
+const port = process.env.PORT || 5000 
+mongoose.connect(process.env.DATABASE_ACCESS, () => {
+    console.log("Data base connected...")
+});
+//Middlewares
+app.use(express.json())
+app.use(cors())
+//API Endpoints
+app.use("/", router)
+app.listen(port, () => console.log("Server is up and running..."))
